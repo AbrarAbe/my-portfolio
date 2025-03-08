e3aad7380d0f5bf78faff2cc933850e9ef1785cd
AbrarAbe <abrar.abe01@gmail.com>
2025-03-08 16:16:39 +0700

    ci: deploy to Github Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://abrarabe.github.io',
+	base: 'my-portfolio',
 	vite: {
 		plugins: [tailwindcss()]
 	}
